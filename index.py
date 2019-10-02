c466a23ce0b65aadc6e570f0a084d098d9d2bb0e
torrias <pilipenkoza@gmail.com>
2019-10-02 18:04:09 +0300

    вычисление примеров

--- index.py
+++ index.py
@@ -1,3 +1,27 @@
-print ("What is your name?")
-name = input()
-print ("Hello", name)
+def arithmetic (first, second, operation):
+    if operation == '*':
+        print(first*second)
+    elif operation == '/':
+        print(first/second)
+    elif operation == '+':
+        print(first+second)
+    elif operation == '-':
+        print(first-second)
+    else:
+        print('Неизвестная операция')
+print ('Введите простой математический пример: ')
+s=input()
+l=len(s)
+i=0
+first_s=''
+while '0'<=s[i]<='9':
+    first_s+=s[i]
+    i+=1
+operation_s=s[i]
+j=i
+i+=1
+second_s=''
+while j<=len(s):
+    second_s+=s[j]
+    j+=1
+arithmetic(int(first_s),int(second_s),operation_s)
